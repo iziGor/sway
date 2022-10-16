e26730f0e65f0a162003bd093fe070369c5d6901
iziGor <A700iGor.gmail.com>
2022-10-16 23:39:10 +0300

    nova skripto por facar skreno-imajo de selektita regiono

---
+++ sway/scripts/get-screenshot-part.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+# vim: autoindent smartindent expandtab smarttab tabstop=2 softtabstop=2 shiftwidth=2 :
+
+SFTMP=/tmp/grim-full.png
+SCTMP=/tmp/grim-clip.png
+grim -l 1 $SFTMP
+#imv -f $SFTMP &
+sxiv -f $SFTMP &
+IMVPID="$!"
+grim -g "$(slurp)" -l 1 $SCTMP
+#imv-msg $IMVPID quit
+kill $IMVPID
+swappy -f $SCTMP
+rm -f $SFTMP $SCTMP
