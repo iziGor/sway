584adbfd25ac207255c2cc6d05da6135d42c1f33
iziGor <A700iGor.gmail.com>
2022-11-22 00:59:38 +0300

    1. ws/window -> bottombar; 2. startigo-skripto por tiling-indicator.py

---
+++ waybar/scripts/run-ti-script.sh
@@ -0,0 +1,11 @@
+#!/bin/env sh
+# vim: autoindent smartindent expandtab smarttab tabstop=2 softtabstop=2 shiftwidth=2 :
+
+cleanup () {
+  [[ -n $PIDP ]] && kill -TERM $PIDP
+}
+
+trap cleanup EXIT
+python -u -OO ${HOME}/.config/waybar/scripts/tiling-indicator.py 2>>$HOME/logs/sway-waybar-ws.log &
+PIDP=$!
+wait $PIDP
