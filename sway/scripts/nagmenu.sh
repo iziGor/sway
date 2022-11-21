9ff7029dd73146d71f7fcfb051b9c8b802d992b0
iziGor <A700iGor.gmail.com>
2022-11-22 00:46:57 +0300

    Chanjo di algoritmo por central alinio di mesajo

--- sway/scripts/nagmenu.sh
+++ sway/scripts/nagmenu.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 # vim:autoindent:smartindent:expandtab:smarttab:tabstop=2:softtabstop=2:shiftwidth=2
 
-PADPREF=''
+#PADPREF=''
 
-padprefix() {
-  for (( i=0; i<$1; i++ )); do
-    PADPREF="$PADPREF "
-  done
-}
+#padprefix() {
+  #for (( i=0; i<$1; i++ )); do
+    #PADPREF="$PADPREF "
+  #done
+#}
 
 DELAY="3"
-MSGWIDTH=38
+#MSGWIDTH=52
 INPMSG="<span color='red'><b>Really exit from sway?</b></span>"
 EXPMSG="sway finos sua laboro pos $DELAY sekundi."
-LENMSG=$(echo -n $EXPMSG | wc -m)
-#echo $LENMSG
-LENMSG=$(( $(( $MSGWIDTH - $LENMSG )) / 2 ))
-#echo $LENMSG
-padprefix $LENMSG
+#LENMSG=$(echo -n $EXPMSG | wc -m)
+#exit
+#padprefix $(( $(( $MSGWIDTH - $LENMSG )) / 2 - 1 ))
 #echo $( echo -n "$PADPREF" | wc -m )
 #EXPMSG="<span color='red'>${PADPREF}${EXPMSG}</span>"
-EXPMSG="<span color='yellow'>${PADPREF}${EXPMSG}</span>"
+#EXPMSG="<span color='yellow'>${PADPREF}${EXPMSG}</span>"
+EXPMSG="<span color='yellow'>${EXPMSG}</span>"
 #retv=$( echo 'Yes|No' | rofi -dmenu -sep '|' -p "" -mesg "$INPMSG" -lines 1 -columns 2 -i -width -30 )
 retv=$( echo 'Yes|No' | rofi -dmenu -sep '|' -p "" -mesg "$INPMSG" -theme-str "listview { lines: 2; }" -i )
 if [[ $retv == Yes ]] ; then
   #coproc rofi -e "$EXPMSG" -markup -width $MSGWIDTH
-  coproc rofi -e "$EXPMSG" -markup -theme-str "#window { width: ${MSGWIDTH}ch; }"
+  coproc rofi -e "$EXPMSG" -markup -theme-str "textbox { horizontal-align: 0.5; }"
   sleep $DELAY
   pkill -f 'python-exec.*i3-cycle-focus'
   pkill -u "${USER}" -x pipewire\|wireplumber 1>/dev/null 2>&1
