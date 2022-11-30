7f1cce20cc1590374418386a51d4c8d2f7b93c86
iziGor <A700iGor.gmail.com>
2022-11-30 21:55:30 +0300

    1. Markizo por faldi 2. Adaptigo di dormigo por 'systemd' 3. Emendo di 'bindsym' por 'stick'-igo di fenestri flotacanta

---
+++ sway/scripts/run-suspend.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# vim:autoindent:smartindent:expandtab:smarttab:tabstop=2:softtabstop=2:shiftwidth=2
+
+PROGCTL=$( command -v systemctl  2>/dev/null)
+
+if [[ -z $PROGCTL ]]; then
+  PROGCTL=$( command -v loginctl  2>/dev/null)
+fi
+
+if [[ -z $PROGCTL ]]; then
+  echo Programo por guvernar dormo-stato ne prezentesas.
+  exit 1
+fi
+
+case "${1@L}" in
+  susp*) SUSPMODE=suspend
+  ;;
+  hiber*) SUSPMODE=hibernate
+  ;;
+  hybri*) SUSPMODE="hybrid-sleep"
+  ;;
+  *) :
+  ;;
+esac
+exec sudo $PROGCTL $SUSPMODE
+#echo exec sudo $PROGCTL $SUSPMODE
