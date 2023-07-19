03816e6877b9b6007f4562dfbeaeb5a1b7025200
iziGor <A700iGor.gmail.com>
2023-07-19 15:56:11 +0300

    sway/scripts/show_scratchp.py: Skripturo por selectar ek celita kladala fenestri adjuntesis

---
+++ sway/scripts/show_scratchp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+# vim: autoindent smartindent expandtab smarttab tabstop=4 softtabstop=4 shiftwidth=4 foldmethod=marker:
+
+from scratchpad_tools_sway_i3ipc import select_from_hidden_sw
+
+
+select_from_hidden_sw()
+
