7f4a6d3f821e776f126767fd2c21d1f78e432e74
iziGor <A700iGor.gmail.com>
2023-07-20 14:54:30 +0300

    sway/scripts/focus-window-2.py: Rilaborita importado; uzesas run() vice check_output()

--- sway/scripts/focus-window-2.py
+++ sway/scripts/focus-window-2.py
@@ -3,12 +3,12 @@
 # vim: autoindent smartindent expandtab smarttab tabstop=4 softtabstop=4 shiftwidth=4 foldmethod=marker:
 
 from argparse import ArgumentParser
-from subprocess import check_output, Popen, CalledProcessError
+from subprocess import run, CalledProcessError
 from os.path import basename
 from sys import exit
-import i3ipc
+from i3ipc import Connection
 
-i3 = i3ipc.Connection()
+i3 = Connection()
 
 def all_windows():
     tree = i3.get_tree()
@@ -20,39 +20,42 @@ def all_windows():
             # con_id = e.id
             aNodes.append({ "name": "{}: {}".format( e.workspace().name, e.name )
                           , "con_id": e.id })
-
     return aNodes
 
 
-if __name__ == '__main__':
+if  __name__ == '__main__':
     parser = ArgumentParser(description = 'Print the names of the focused window of each workspace.')
     parser.add_argument('--menu', default='rofi', help='The menu command to run (ex: --menu=dmenu)')
     # parser.parse_args()
     (args, menu_args) = parser.parse_known_args()
 
-    if len(menu_args) and menu_args[0] == '--':
+    if  len(menu_args) and menu_args[0] == '--':
         menu_args = menu_args[1:]
 
     win = ''
     aNodes = all_windows()
 
     # set default menu args for supported menus
-    if basename(args.menu) == 'dmenu':
-        menu_args += ['-i', '-f']
+    if  basename(args.menu) == 'dmenu':
+        menu_args += [ '-i', '-f' ]
     elif basename(args.menu) == 'rofi':
-        menu_args += ['-show', '-dmenu', '-p', 'Win: ', '-theme-str', 'listview {{ lines: {}; }}'.format( min( len(aNodes), 10 ) )]
-
+        menu_args += [ '-show', '-dmenu', '-p', 'Win: '
+                     , '-theme-str', 'listview {{ lines: {}; }}'.format( min( len(aNodes), 10 )) ]
     try:
-        win = check_output([args.menu] + menu_args, input=bytes('\n'.join([e.get("name") for e in aNodes]),
-                'UTF-8')).decode('UTF-8').strip()
+        win = run( [args.menu] + menu_args
+                 , input='\n'.join([ e.get("name") for e in aNodes ])
+                 , check=True
+                 , capture_output=True
+                 , encoding='UTF-8'
+                 ).stdout.strip()
     except CalledProcessError as e:
         exit(e.returncode)
 
-    if not win:
-        # nothing to do
-        exit(0)
+    if  win:
+        for e in aNodes:
+            if  win == e.get("name").strip():
+                i3.command("[con_id=%s] focus" % e.get("con_id"))
+                break
+    # nothing to do
+    exit(0)
 
-    for e in aNodes:
-        if  win == e.get("name").strip():
-            i3.command("[con_id=%s] focus" % e.get("con_id"))
-            break
