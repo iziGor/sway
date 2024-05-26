e68c84434ef31763b29c84386867d61f0008f135
iziGor <A700iGor.gmail.com>
2024-05-26 12:13:42 +0300

    sw/scripts/run-session.sh: reorder starting units

--- sway/scripts/run-session.sh
+++ sway/scripts/run-session.sh
@@ -3,10 +3,10 @@
 
 if ( command -v systemctl >/dev/null 2>&1 && systemctl --user list-jobs >/dev/null 2>&1 ); then
   # nexta du instrucioni dev esar nur ye un lineo nam li dev startesar unu pos altra
-  systemctl --user import-environment {,WAYLAND_}DISPLAY SWAYSOCK; systemctl --user start sway-session.target
-  swaymsg -t subscribe '["shutdown"]' && systemctl --user stop sway-session.target
-  systemctl --user start dunst.service
+  systemctl --user import-environment {,WAYLAND_}DISPLAY SWAYSOCK; systemctl --user start sway-session.target; systemctl --user start dunst.service
+  # systemctl --user start dunst.service
   # foot-server стартует через `systemd --user enable foot...`
+  swaymsg -t subscribe '["shutdown"]' && systemctl --user stop sway-session.target
 else
   dunst &
   foot -s &
