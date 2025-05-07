7daeaf2fec096745bdc1aa378a148d74e30cf05c
iziGor <A700iGor.gmail.com>
2025-05-07 13:13:45 +0300

    sw: starting of foot-sever with systemd-run

--- sway/scripts/run-foot.sh
+++ sway/scripts/run-foot.sh
@@ -3,4 +3,6 @@
 
 if ! ( command -v systemctl >/dev/null 2>&1 && systemctl --user list-jobs >/dev/null 2>&1 ); then
   exec foot -s &
+else
+  exec systemd-run --user --no-block --unit=foot foot --server
 fi
--- sway/scripts/run-session.sh
+++ sway/scripts/run-session.sh
@@ -6,7 +6,9 @@ SWAYSCRIPTDIR="${XDG_CONFIG_HOME:-$HOME/.config}/sway/scripts"
 if ( command -v systemctl >/dev/null 2>&1 && systemctl --user list-jobs >/dev/null 2>&1 ); then
   # nexta du instrucioni dev esar nur ye un lineo nam li dev startesar unu pos altra
   # systemctl --user import-environment {,WAYLAND_}DISPLAY SWAYSOCK; systemctl --user start sway-session.target; systemctl --user start dunst.service
-  # # foot-server стартует через `systemd --user enable foot...`
+  # # foot-server стартует через `systemctl --user enable foot...`
+  # или через systemd-run
+  # systemd-run --user --no-block --unit=foot foot --server
   # swaymsg -t subscribe '["shutdown"]' && systemctl --user stop sway-session.target
 
   # запускаем сессию, используя инструменты, взятые с https://github.com/alebastr/sway-systemd
