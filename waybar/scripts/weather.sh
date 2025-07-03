2b237bcfa7a66a00bdd510ba64d281b109d33b52
iziGor <A700iGor.gmail.com>
2025-07-03 15:06:32 +0300

    wb/scripts/weather.sh: logiko di aceso ad wttr.in chanjesis

--- waybar/scripts/weather.sh
+++ waybar/scripts/weather.sh
@@ -51,8 +51,11 @@ fi
 cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 if [[ $cacheage -gt 1740 ]] || [[ ! -s $cachedir/$cachefile ]]; then
 
-  if ping -qc1 wttr.in >/dev/null 2>&1 ; then
-    TMPOUT=$(curl -s "https://wttr.in/$location?format=1")
+  # disabled because 'ping' cannot resolve the name of the resource
+  # deskapabligita nam programo 'ping' falias resolvar/obtenar adreso di situo
+  # if ping -qc1 wttr.in >/dev/null 2>&1 ; then
+    # TMPOUT=$(curl -s "https://wttr.in/$location?format=1")
+    TMPOUT=$(curl --connect-timeout 15 -s "http://wttr.in/$location?format=1")
     # curl -s "https://wttr.in/$location?format=1" >$cachedir/$cachefile
     if [[ -n "$TMPOUT" ]] ; then
       case "$TMPOUT" in
@@ -61,7 +64,8 @@ if [[ $cacheage -gt 1740 ]] || [[ ! -s $cachedir/$cachefile ]]; then
           ;;
         *)
           echo "$TMPOUT" | tr -s '[:blank:]' >"$cachedir"/"$cachefile"
-          curl -s "https://ru.wttr.in/$location?0qT" |
+          # curl -s "https://ru.wttr.in/$location?0qT" |
+          curl --connect-timeout 15 -s "http://ru.wttr.in/$location?0qT" |
             sed 's/\\/\\\\/g' |
               sed ':a;N;$!ba;s/\n/\\n/g' |
                 sed 's/"/\\"/g' > "$cachedir"/"$cachefile_tt"
@@ -70,7 +74,7 @@ if [[ $cacheage -gt 1740 ]] || [[ ! -s $cachedir/$cachefile ]]; then
     else
       true
     fi
-  fi
+  # fi
 fi
 
 ## Restore IFSClear
