193d433d9834acb721970c3e8935d1652a24ab55
iziGor <A700iGor.gmail.com>
2025-09-22 11:07:06 +0300

    sw/scripts/empty_workspace.sh: de nun egardesas komplexa nomi dil laborspaci

--- sway/scripts/empty_workspace.sh
+++ sway/scripts/empty_workspace.sh
@@ -3,7 +3,7 @@
 
 MAX_DESKTOPS=20
 WORKSPACES=$(seq -s '\n' 1 1 ${MAX_DESKTOPS})
-EMPTY_WORKSPACE=$( (swaymsg -pt get_workspaces | awk -e '/Workspace [0-9]+ /{print int($2)}' ; \
-  echo -e ${WORKSPACES} ) | sort -n | uniq -u | head -n 1)
+EMPTY_WORKSPACE=$( (swaymsg -pt get_workspaces | awk -e '/Workspace [0-9]+/ {print gensub(/Workspace ([0-9]+).*$/, "\\1", 1)}' ; \
+  echo -e "${WORKSPACES}" ) | sort -n | uniq -u | head -n 1)
 
-swaymsg workspace ${EMPTY_WORKSPACE}
+swaymsg workspace "${EMPTY_WORKSPACE}"
